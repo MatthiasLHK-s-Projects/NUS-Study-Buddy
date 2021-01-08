b6e7aeeaac0a6fddc898b1cf8bb2c505d6f498e0
murtubak <murtubakchickenss@gmail.com>
2021-01-09 03:24:17 +0800

    Add cute kitten

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './HomeScreen.js';
+import ModuleScreen from './modules/ModuleScreen.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <ModuleScreen />
       <StatusBar style="auto" />
     </View>
   );
